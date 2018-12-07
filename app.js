d48132502136d44cc39783254c4ac67d82b64f86
LucasJunio <lucas.verdine@gmail.com>
2018-12-06 23:39:06 -0200

    port 443

--- app.js
+++ app.js
@@ -53,6 +53,6 @@ app.use('/auth', authRoutes);
 
 app.use('/', express.static(__dirname + '/public/build/'));
 
-app.listen(3001, function () {
+app.listen(443, function () {
 	console.log('Server running at http://localhost:3001');
 });
